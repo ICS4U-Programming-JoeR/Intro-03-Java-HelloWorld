4d18490e4b50858c6663bfc9e890a69543982a34
Joe Rugezo <joedassin.rugezo@stu.ocsb.ca>
2025-02-12 15:16:47 +0000

    adding the code to the program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+* This program will print out Hello world, this is Joe
+*
+* @author  Joe Rugezo
+* @version 1.0
+* @since   2025-02-11
+*/
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world this is Joe");
+    }
+}
